5b4ec1c95ea24eef6126311bdaa905f44283e3a6
Debasish Sahoo <dsahoo@nexient.com>
2022-06-15 11:50:40 -0400

    Remove commented code

--- local.tf
+++ local.tf
@@ -1,35 +1,4 @@
 locals {
-    # resource_types = {
-    #     "resource_group" = {
-    #     type           = "rg"
-    #     maximum_length = 63
-    #     }
-    #     "app_insights" = {
-    #     type           = "appins"
-    #     maximum_length = 260
-    #     }
-    #     "service_plan" = {
-    #     type           = "plan"
-    #     maximum_length = 60
-    #     }
-    #     "log_analytics" = {
-    #     type           = "log"
-    #     maximum_length = 63
-    #     }
-    #     "storage_account" = {
-    #     type           = "sa"
-    #     maximum_length = 24
-    #     }
-    #     "function_app" = {
-    #     type           = "fn"
-    #     maximum_length = 59
-    #     }
-    #     "key_vault" = {
-    #     type           = "kv"
-    #     maximum_length = 24
-    #     }
-    # }
-
   resource_group = {
     location = var.resource_group.location
     name = module.resource_group.resource_group.name
@@ -58,6 +27,7 @@ locals {
   key_vault_name                = module.resource_name["key_vault"].recommended_per_length_restriction
   storage_account_name          = module.resource_name["storage_account"].lower_case
 
+  # Get the corresponding objectIds of all the deployment slots defined in variables
   slots_object_id_map = {
     for name in var.deployment_slots :
       name => module.function_app.function_app_linux_slots["${name}"].identity[0].principal_id
@@ -65,6 +35,7 @@ locals {
 
   all_identities_object_id_map = merge({"production" : module.function_app.function_app_identity.principal_id}, local.slots_object_id_map)
 
+  # Key vault Access policies map for all deployment slots
   key_vault_access_policies     = {
     for name, id in local.all_identities_object_id_map:
 
